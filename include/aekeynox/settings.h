4cab7287a5e2bc9ac8c70dd6fcf6c1e35b517995
Gerry Agbobada <gerry@tsuga.com>
2026-05-17 15:49:34 +0200

    Try ækeynox module

---
+++ include/aekeynox/settings.h
@@ -0,0 +1,8 @@
+// Selenium configuration for Totem keyboard
+// Host: Ergol layout on Linux
+// Flavor: HRM (default, no need to uncomment)
+// Variant: Vim navigation
+
+#define KB_LAYOUT_ERGOL
+#define LINUX
+#define VIM_NAVIGATION
